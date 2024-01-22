bcff112cd6c9f9da2767f7a9284661b618dda82c
JCCode288 <jendycaprico@gmail.com>
2024-01-22 10:19:25 +0700

    fix: wrong color on dark mode

--- resources/views/components/register/form.blade.php
+++ resources/views/components/register/form.blade.php
@@ -11,41 +11,40 @@
                 @csrf
                 <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 w-fit h-fit">
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="email">Email Address</label>
                         <input id="email" type="email" name="email"
-                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-50 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                            placeholder="Email Address" />
                         @error('email')
                             <x-exception.text :message="$message" />
                         @enderror
                     </div>
 
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="name">Name</label>
                         <input id="name" type="name" name="name"
-                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-50 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                            placeholder="Name" />
                         @error('name')
                             <x-exception.text :message="$message" />
                         @enderror
                     </div>
 
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="password">Password</label>
                         <input id="password" type="password"
-                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-                            name="password">
+                            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-50 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                            name="password" placeholder="Password" />
                         @error('password')
                             <x-exception.text :message="$message" />
                         @enderror
                     </div>
                 </div>
 
-                <a class="text-gray-700 dark:text-gray-200" href="/login"></a>
-
                 <div class="flex justify-end mt-6">
                     <button
                         class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                         type="submit">Register</button>
                 </div>
+
+
             </form>
         </div>
 
