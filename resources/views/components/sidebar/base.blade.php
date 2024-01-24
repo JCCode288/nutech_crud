30e871a09d5c6d5282af2398095fd23475adb8af
JCCode288 <jendycaprico@gmail.com>
2024-01-24 22:24:10 +0700

    fix: hotfixing logo sidebar

--- resources/views/components/sidebar/base.blade.php
+++ resources/views/components/sidebar/base.blade.php
@@ -2,6 +2,11 @@
     class="flex sticky flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-[#F13B2F] border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
 
     <div class="flex flex-col justify-between flex-1 mt-6">
+        <div class="flex w-full text-gray-100 text-lg font-semibold justify-center gap-2 align-center">
+            <img class="antialiased" src="{{ @asset('/storage/handbag.png') }}" />
+            <h2>SIMS Web App</h2>
+        </div>
+        <hr class="my-4 border-gray-200 dark:border-gray-600" />
         <x-sidebar.nav />
         @auth
             <a href="#" class="flex relative flex-col items-start mx-1 antialiased">
