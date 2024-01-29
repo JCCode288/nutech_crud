ecd6f49f84ea1850b7ea004801ffa39e5c37cc43
JCCode288 <jendycaprico@gmail.com>
2024-01-29 19:01:29 +0700

    chore: cleanup base components and pages

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+
+// const user = ref(auth.user);
+</script>
+
+<template>
+    <aside
+        class="flex sticky flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-[#F13B2F] border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700"
+    >
+        <div class="flex flex-col justify-between flex-1 mt-6">
+            <div
+                class="flex w-full text-gray-100 text-lg font-semibold justify-center gap-2 align-center"
+            >
+                <img
+                    class="antialiased"
+                    src="{{ @asset('/storage/handbag.png') }}"
+                />
+                <h2>SIMS Web App</h2>
+            </div>
+            <hr class="my-4 border-gray-200 dark:border-gray-600" />
+            <SidebarNav />
+            <a
+                v-if="$page.props.auth.user.name"
+                href="#"
+                class="flex relative flex-col items-start mx-1 antialiased"
+            >
+                <small class="mx-2 font-medium text-gray-200"
+                    >Logged User</small
+                >
+                <span class="mx-2 font-medium text-gray-200">{{
+                    $page.props.auth.user.name
+                }}</span>
+            </a>
+        </div>
+    </aside>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,5 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import Welcome from "@/Components/Welcome.vue";
 import { onMounted } from "vue";
 
 const { products, categories, nextPage, prevPage } = defineProps([
@@ -16,17 +15,11 @@ onMounted(() => {
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
+    <AppLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <div>Test</div>
                 </div>
             </div>
         </div>
